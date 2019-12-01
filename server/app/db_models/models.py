133fa619ada8d79add820e6ee2eeedbcfefefdf6
Jonatha-Varjao <jonathavarjao@gmail.com>
2019-11-30 23:59:36 -0300

    (feat): adding boolean is_completed field into models

--- server/app/db_models/models.py
+++ server/app/db_models/models.py
@@ -12,13 +12,15 @@ class Task(Base, Timestamp):
     __tablename__ = 'tasks'
     id = Column('id', String(36), default=lambda: str(uuid.uuid4()), primary_key=True)
     name = Column(String(25))
-    
+    is_completed = Column(Boolean, unique=False, default=False)
+
+    actions = relationship("Action", back_populates="tasks")
 
 class Action(Base, Timestamp):
     __tablename__ = 'actions'
     id = Column('id', String(36), default=lambda: str(uuid.uuid4()), primary_key=True)
     name = Column(String(25))
     task_id = Column(ForeignKey('tasks.id'))
-    
+    is_completed = Column(Boolean, unique=False, default=False)
+
     task_parent = relationship('Task', back_populates="actions")
-    

4e9d212290194c2e3d0d0a788e32734686cc9350
Jonatha-Varjao <jonathavarjao@gmail.com>
2019-11-30 23:57:28 -0300

    Task crud complete

---
+++ server/app/crud/action.py
@@ -0,0 +1,14 @@
+from typing import List, Optional
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+
+def create_action():
+    pass
+
+def complete_action():
+    pass
+
+def delete_action():
+    pass
---
+++ server/app/crud/task.py
@@ -0,0 +1,68 @@
+from typing import List, Optional
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from server.app.db_models.models import Task, Action
+from server.app.schema.task import TaskCreate, TaskInDB, TaskUpdate
+
+def get_tasks(
+        db: Session,
+        *,
+        skip: int = 0,
+        limit: int = 0,
+        is_completed: bool = False
+) -> List[Optional[Task]]:
+    task = db.query(Task).filter(Task.is_completed == is_completed)\
+        .order_by(Task.name).offset(skip).limit(limit)
+    task.actions
+    return task
+
+def create_task(
+    db: Session,
+    *,
+    task_in: TaskCreate
+) -> Task :
+    task_data = jsonable_encoder(task_in)
+    task = Task(**task_data)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+    return task
+
+def update_task(
+    db: Session,
+    *,
+    task_on: Task,
+    task_in: TaskUpdate,
+) -> Task :
+    task_data = jsonable_encoder(task_on)
+    update_data = task_in.dict(skip_defaults=True)
+    for field in task_data:
+        if field in update_data:
+            setattr(task_on, field, update_data[field])
+    db.add(task_on)
+    db.commit()
+    db.refresh(task_on)
+    return task_on
+
+def complete_task(
+    db: Session,
+    *,
+    id: str
+) -> Task :
+    task = db.query(Task).filter(Task.id == id).first()
+    task.is_completed = True
+    db.commit()
+    db.refresh(task)
+    return task
+
+def delete_task(
+    db: Session,
+    *,
+    id: str
+) -> Task:
+    task = db.query(Task).filter(Task.id == id).first()
+    db.delete(task)
+    db.commit()
+    return task

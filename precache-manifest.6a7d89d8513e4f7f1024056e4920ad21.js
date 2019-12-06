43a12878ebede59023bed9e644e43c28fdd18ef6
Jonatha-Varjao <jonathavarjao@gmail.com>
2019-12-06 12:45:47 -0300

    Updates

---
+++ precache-manifest.6a7d89d8513e4f7f1024056e4920ad21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3898851817c9a561d21050f9f42fa0c",
+    "url": "/TODO/index.html"
+  },
+  {
+    "revision": "4302e2d7bc9a581cd6ef",
+    "url": "/TODO/static/css/main.de72ec8c.chunk.css"
+  },
+  {
+    "revision": "08119574c104f13d2a54",
+    "url": "/TODO/static/js/2.86f1969f.chunk.js"
+  },
+  {
+    "revision": "4302e2d7bc9a581cd6ef",
+    "url": "/TODO/static/js/main.5ebbb62f.chunk.js"
+  },
+  {
+    "revision": "b52077bde8e1ee0e4a4e",
+    "url": "/TODO/static/js/runtime-main.ca0b5a6e.js"
+  }
+]);
